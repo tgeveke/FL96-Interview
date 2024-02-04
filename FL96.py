ca56af48f72dd2096ea97fb52861a81e7fcc7f38
Tom Geveke <81868434+tgeveke@users.noreply.github.com>
2024-02-04 11:09:21 -0500

    Add unit tests

--- FL96.py
+++ FL96.py
@@ -7,6 +7,14 @@
 
 # ---------- Imports ---------- #
 import csv
+import math
+
+# ---------- Custom Exceptions ---------- #
+class InvalidWorkflowException(RuntimeError):
+    pass
+
+class PrecursorMissingException(RuntimeError):
+    pass
 
 # ---------- Classes ---------- #
 class Material:
@@ -17,7 +25,9 @@ class Precursor(Material):
     def __init__(self) -> None:
         super().__init__()
         self.concentration: int or float
-
+    
+    def __str__(self) -> str:
+        return f'Precursor of element {self.precursor} at location: {self.location} with concentration {self.concentration} M/L'
 
 class Target(Material):
     def __init__(self) -> None:
@@ -28,6 +38,10 @@ class Target(Material):
 
         self.current_volume = 0
         self.current_makeup = {}
+
+    def __str__(self) -> str:
+        return f'Target with makeup {self.target_ratio} at location: {self.location} with target volume {self.target_volume} mL'
+
 
     def add(self, element, moles, volume):
         if element in self.current_makeup:
@@ -59,141 +73,180 @@ class Target(Material):
 
         self.target_ratios = quantities
         return self.target_ratios
-
-
-# ---------- Methods ---------- #
-def find_element(element, precursors) -> Precursor:
-    for precursor in precursors:
-        if precursor.precursor == element:
-            return precursor
-    return None
-
-def calculate_workflow_steps(precursors: list[Precursor], targets: list[Target]) -> list:
-    steps = []
     
-    for target in targets:
-        target.parse_target_ratio()
+class AssayGenerator:
+    def __init__(self, 
+                 precursors_fname: str = 'precursors.csv',
+                 targets_fname: str = 'targets.csv',
+                 output_fname: str = 'assays/assay.txt') -> None:
+        self.output_fname = output_fname
 
-        target.target_volume = float(target.target_volume)
-        target_volume_ml = target.target_volume
+        self.precursors = self.process_csv_into_objects(precursors_fname, Precursor)
+        self.targets = self.process_csv_into_objects(targets_fname, Target)
+        self.steps = []
+        self.calculate_workflow_steps()
+        self.generate_workflow_script()
 
-        for element, ratio in target.target_ratios.items():
-            precursor = find_element(element, precursors)
-            if precursor is None:
-                raise RuntimeError(f'No precusor for {element} found')
-            
-            concentration_mol_per_l = float(precursor.concentration)
-
-            # Calculate volumes and ratios based on the modified algorithm
-            target_moles = ratio * (target_volume_ml / 1000)  # Convert target volume to liters
-            volume_l = target_moles / concentration_mol_per_l
-            volume_ml = round(volume_l * 1000, 3)
-
-            steps.append([precursor.location, target.location, volume_ml])
-
-    if not check_workflow(steps, precursors, targets):
-        raise RuntimeError('Workflow check returned False')
-    
-    return steps
-
-def check_workflow(steps, precursors, targets) -> bool:
-    def get_precursor_at(location) -> Precursor:
-        for precursor in precursors:
-            if precursor.location == location:
-                print(f'precursor at location: {location} = {precursor.precursor}')
+    def find_element(self, element) -> Precursor:
+        for precursor in self.precursors:
+            if precursor.precursor == element:
                 return precursor
         return None
-            
-    def get_target_at(location) -> Target:
-        for target in targets:
+    
+    def get_precursor_at(self, location) -> Precursor:
+            for precursor in self.precursors:
+                if precursor.location == location:
+                    return precursor
+            return None
+                
+    def get_target_at(self, location) -> Target:
+        for target in self.targets:
             if target.location == location:
-                print(f'target at location: {location} = {target.target_ratios}')
                 return target
         return None
 
-    for step in steps:
-        start_location, end_location, volume = step
-        precursor = get_precursor_at(start_location)
-        target = get_target_at(end_location)
+    def calculate_workflow_steps(self) -> list:
+        steps = []
+        for target in self.targets:
+            target.parse_target_ratio() # Step which converts string makeup eg "Fe0.5Zn0.5" to dictionary {"Fe": 0.5, "Zn": 0.5}
+            target.target_volume = float(target.target_volume)
+            target_volume_ml = target.target_volume
 
-        element = precursor.precursor
-        moles = float(precursor.concentration) * volume
+            target_sum_volume = 0
+            precursor_volumes = {}
+            precursor_ratios = {}
+            for element, target_ratio in target.target_ratios.items():
+                precursor = self.find_element(element)
+                if precursor is None:
+                    raise PrecursorMissingException(f'Precursor {element} is missing, which is needed in {target}')
+                concentration_mol_per_l = float(precursor.concentration)  # Concentration in mol/L
+                volume_l = target_ratio / concentration_mol_per_l  # Convert moles to volume in liters
+                volume_ml = volume_l * 1000  # Convert volume to mL
+                precursor_volumes[element] = volume_ml
+                precursor_ratios[element] = target_ratio
 
-        target.add(element, moles, volume)
+                target_sum_volume += volume_ml
 
-        # input('next step?')
+            # Scale volumes and ratios to meet the desired total volume
+            scale_factor = target_volume_ml / target_sum_volume
+            scaled_sum_volumes = 0
+            scaled_ratios = {}
+            for element in precursor_volumes:
+                precursor = self.find_element(element)
+                precursor_volumes[element] *= scale_factor
+                scaled_sum_volumes += precursor_volumes[element]
+                scaled_ratios[element] = precursor_ratios[element] * scale_factor
 
-    for target in targets:
-        print(target.target_ratios)
-        print(target.current_makeup)
-        if target.current_volume != target.target_volume:
-            print(f'current volume {target.current_volume} != target volume {target.target_volume}')
-        for element, target_ratio in target.target_ratios.items():
-            print(f'Checking {element} with ratio: {target_ratio}')
-            if element not in target.current_makeup.keys():
-                print(f'{element} not in {target.current_makeup.keys()}')
-                return False
-            if abs(target_ratio - target.current_makeup[element] / target.target_volume) > 1e-5:
-                print(f'{element} target {target_ratio} != current {target.current_makeup[element] / target.current_volume}')
-                return False
-        break
-    
-    return True
+                steps.append([precursor.location, target.location, round(precursor_volumes[element], 4)])
+            
+            # Sanity check for target volume
+            if not math.isclose(scaled_sum_volumes, target.target_volume, rel_tol=0.01):
+                print(f'{scaled_sum_volumes, target.target_volume}')
 
-def generate_workflow_script(steps, output_fname: str) -> None:
-    def generate_step_string(start_location: str, end_location: str, quantity: float or int):
-        return f'transfer("{start_location}", "{end_location}", {quantity})\n'
-    
-    steps_strings = [
-        generate_step_string(*step) for step in steps
-    ]
-    
-    with open(output_fname, 'w') as output_file:
-        output_file.writelines(steps_strings)
+        self.steps = steps
 
-    print(f'Successfully saved robot script at: {output_fname}')
-    with open(output_fname, 'r') as output_file:
-        [print(f'\t{line}') for line in output_file.readlines()]
-    
+        # Run check_workflow(), which validates that running all generated steps will produce the correct targets
+        workflow_success, workflow_message = self.check_workflow()
+        if not workflow_success:
+            raise InvalidWorkflowException(workflow_message)
+        
+        return steps
+        
+    def check_workflow(self) -> (bool, str):
+        ''''
+        Validates
+        '''
 
-def process_csv_into_objects(fname: str, object_class) -> list:
-    objects = []
-    with open(fname, 'r', encoding='utf-8-sig') as csv_file:
-        csv_reader = csv.reader(csv_file)
+        for target in self.targets:
+            target.current_makeup = {}
+            target.current_volume = 0
 
-        for row_id, row in enumerate(csv_reader):
-            if row_id == 0:
-                attributes = row
-            else:
-                myobject = object_class()
-                for attribute_id, attribute_name in enumerate(attributes):
-                    attribute_name = attribute_name.lower()
-                    attribute_name = attribute_name.split(' (')[0]
-                    attribute_name = attribute_name.replace(' ', '_')
-                    attribute_value = row[attribute_id]
-                    setattr(myobject, attribute_name, attribute_value)
-                objects.append(myobject)
-        return objects
+        for step in self.steps:
+            start_location, end_location, volume = step
+            precursor = self.get_precursor_at(start_location)
+            if precursor is None:
+                return False, f'No precursor at {start_location}'
+            target = self.get_target_at(end_location)
+            if target is None:
+                return False, f'No target at {end_location}'
 
-def main():
-    # precursors_fname = 'precursors.csv'
-    precursors_fname = r'/Users/tom/Downloads/FL96/precursors.csv'
-    targets_fname = r'/Users/tom/Downloads/FL96/targets.csv'
-    output_fname = 'assay.txt'
+            element = precursor.precursor
+            moles = float(precursor.concentration) * volume
 
-    precursors = process_csv_into_objects(precursors_fname, Precursor)
-    targets = process_csv_into_objects(targets_fname, Target)
+            target.add(element, moles, volume)
 
-    steps = calculate_workflow_steps(
-        precursors=precursors,
-        targets=targets,
-    )
-    generate_workflow_script(
-        steps=steps,
-        output_fname=output_fname
-    )
+        for target in self.targets:
+            ratios = {element: value / sum(target.current_makeup.values()) for element, value in target.current_makeup.items()}
+            if not math.isclose(target.current_volume, target.target_volume, rel_tol=0.01):
+                return False, f'current volume {target.current_volume} != target volume {target.target_volume} for {target}'
+            for element, target_ratio in target.target_ratios.items():
+                if element not in ratios.keys():
+                    return False, f'{element} not in {ratios.keys()} for target {target}'
+                if not math.isclose(target_ratio, ratios[element], rel_tol=0.01):
+                    return False, f'{element} target {target_ratio} != current {ratios[element]} for {target}'
+        
+        return True, 'Workflow is valid'
+
+    def generate_workflow_script(self) -> None:
+        def generate_step_string(start_location: str, end_location: str, quantity: float or int):
+            return f'transfer("{start_location}", "{end_location}", {quantity})\n'
+        
+        steps_strings = [
+            generate_step_string(*step) for step in self.steps
+        ]
+        
+        with open(self.output_fname, 'w') as output_file:
+            output_file.writelines(steps_strings)
+
+        print(f'Successfully saved robot script at: {self.output_fname}')
+        with open(self.output_fname, 'r') as output_file:
+            for line in output_file.readlines():
+                print(f'\t {line}')
+        
+    @staticmethod
+    def process_csv_into_objects(fname: str, object_class) -> list:
+        objects = []
+        with open(fname, 'r', encoding='utf-8-sig') as csv_file: # Encoding is required due to "ufeff" in input csv
+            csv_reader = csv.reader(csv_file)
+
+            for row_id, row in enumerate(csv_reader):
+                if row_id == 0:
+                    attributes = row
+                else:
+                    myobject = object_class()
+                    for attribute_id, attribute_name in enumerate(attributes):
+                        attribute_name = attribute_name.lower()
+                        attribute_name = attribute_name.split(' (')[0]
+                        attribute_name = attribute_name.replace(' ', '_')
+                        attribute_value = row[attribute_id]
+                        setattr(myobject, attribute_name, attribute_value)
+                    objects.append(myobject)
+            return objects
 
 if __name__ == '__main__':
     import os
+    import argparse
     os.system('clear')
-    main()
+
+    parser = argparse.ArgumentParser(description='Process precursors and targets for liquid handling automation')
+    parser.add_argument('--precursors_path', help='Path to the precursor CSV file')
+    parser.add_argument('--targets_path', help='Path to the target CSV file')
+    parser.add_argument('--run_tests', action='store_true', help='Run all available test cases')
+
+    args = parser.parse_args()
+
+    if args.run_tests:
+        os.system('python3 -m unittest tests.tests')
+    else:
+        if not args.precursors_path or not args.targets_path:
+            precursors_fname = 'precursors.csv'
+            targets_fname = 'targets.csv'
+        else:
+            precursors_fname = args.precursor_path
+            targets_fname = args.target_path
+
+        # Call main script
+        AssayGenerator(
+            precursors_fname=precursors_fname,
+            targets_fname=targets_fname
+        )
--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,78 @@
 import unittest
+import os
 import FL96
 
-class CSVTests(unittest.TestCase):
-    def test_read_csv(self):
-        csv_ret = FL96.process_csv_into_objects(fname='test_csv.csv', object_class=FL96.Target)
-        assert csv_ret
+class CSVTests(unittest.TestCase):    
+    @staticmethod
+    def generate_test_csv():
+        pass
 
-class AssayTests(unittest.TestCase):
-    pass
+    def test_read_csv(self):
+        csv_ret = FL96.AssayGenerator.process_csv_into_objects(fname='targets.csv', object_class=FL96.Target)
+        assert isinstance(csv_ret[0], FL96.Target)
+
+        csv_ret = FL96.AssayGenerator.process_csv_into_objects(fname='precursors.csv', object_class=FL96.Precursor)
+        assert isinstance(csv_ret[0], FL96.Precursor)
+
+class AssayGeneratorTests(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.assay_generator = FL96.AssayGenerator()
+    
+    def test_calculate_workflow_steps(self):
+        self.assay_generator.calculate_workflow_steps()
+        assert self.assay_generator.steps != []
+
+    def test_find_element(self):
+        precursor = self.assay_generator.find_element(element=self.assay_generator.precursors[0].precursor)
+        assert precursor == self.assay_generator.precursors[0]
+
+    def test_get_precursor_at(self):
+        precursor = self.assay_generator.get_precursor_at(location=self.assay_generator.precursors[0].location)
+        assert precursor == self.assay_generator.precursors[0]
+
+    def test_get_target_at(self):
+        target = self.assay_generator.get_target_at(location=self.assay_generator.targets[0].location)
+        assert target == self.assay_generator.targets[0]
+
+    def test_missing_precursor(self):
+        prev_precursors = self.assay_generator.precursors
+        self.assay_generator.precursors = prev_precursors.copy()
+        self.assay_generator.precursors.pop(0)
+
+        self.assertRaises(FL96.PrecursorMissingException, self.assay_generator.calculate_workflow_steps)
+
+        self.assay_generator.precursors = prev_precursors
+
+    def test_script_generation(self):
+        self.assay_generator.output_fname = 'testing_assay.txt'
+        prev_steps = self.assay_generator.steps
+        self.assay_generator.steps = [
+            ['A1', 'B1', 1.0],
+            ['A1', 'B2', 2.0]
+        ]
+        self.assay_generator.generate_workflow_script()
+
+        with open('testing_assay.txt', 'r') as file:
+            lines = file.readlines()
+            assert lines[0] == 'transfer("A1", "B1", 1.0)\n'
+            assert lines[1] == 'transfer("A1", "B2", 2.0)\n'
+
+        os.system('rm testing_assay.txt')
+        self.assay_generator.steps = prev_steps
+
+class TargetTests(unittest.TestCase):
+    def test_parse_target_ratio(self):
+        target = FL96.Target()
+        target.target_ratio = 'Fe0.5Zn0.5'
+        target.parse_target_ratio()
+        assert target.target_ratios == {'Fe': 0.5, 'Zn': 0.5}
+    
+    def test_add(self):
+        target = FL96.Target()
+        target.add(element='Zn', moles=1, volume=1.5)
+        assert target.current_volume == 1.5
+        assert target.current_makeup == {'Zn': 1.0}
 
 if __name__ == '__main__':
     unittest.main()
